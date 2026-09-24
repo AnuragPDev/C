170fe0f860e7c3e2e2661578413f94963f1f634b
Anurag Pant <anuragp.dev@gmail.com>
2026-09-25 00:04:13 +0530

    DO_While

---
+++ doWhile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+// dop while loop is the other form of while loop where we first execute the do block and then while loop
+int num=1;
+do {
+printf("Enter a number greater than 0: ");
+scanf("%d",&num);
+
+
+
+
+}
+while(num<=0);
+	return 0;
+}
