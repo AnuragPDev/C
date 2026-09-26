7e821f92e382c8e750b2a6fa75c3d83fadf3a685
Anurag Pant <anuragp.dev@gmail.com>
2026-09-26 12:10:44 +0530

    Print in c

---
+++ MiniProjects/for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// for loop repeats the code a limited number of time 
+int main(){
+for (int i =10; i>=0; i--){
+
+	printf("%d\n",i);
+
+
+
+}
+printf("Happy New Year\n");
+return 0;
+}
